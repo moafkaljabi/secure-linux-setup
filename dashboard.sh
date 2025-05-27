e8e5eae5305204f5edcc31d1ed6fabc488a40451
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-05-27 11:58:43 +0200

    Starting with the dashboard

---
+++ dashboard.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+
+echo "==============================================="
+echo "      🛡️  Secure Linux CLI Dashboard"
+echo "==============================================="
+
+
+# Uptime and Load
+echo -e "\nUptime and Load:"
+uptime
+
+# Logged in users 
+echo "\nLogged in users:"
+who
